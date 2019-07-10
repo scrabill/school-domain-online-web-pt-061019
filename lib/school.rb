07992cad32b9ac1cc3ec50ceb56d232d4ba38606
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-10 22:24:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,26 +3,28 @@ require "pry"
 # code here!
 class School
 
+  attr_accessor :name, :roster
+
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def roster=(roster)
-    @roster = roster
-  end
-
-  def roster
-    @roster
-  end
+  # def name=(name)
+  #   @name = name
+  # end
+  #
+  # def name
+  #   @name
+  # end
+  #
+  # def roster=(roster)
+  #   @roster = roster
+  # end
+  #
+  # def roster
+  #   @roster
+  # end
 
   def add_student(name,grade)
 
