14d0f0862e3f45b596120b221c2ca5cf3b3a1c43
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-10 22:23:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -41,10 +41,19 @@ class School
   end
 
   def sort
+<<<<<<< HEAD
     # roster.sort
     # binding.pry
     roster.each do |key,value| # For each key, take the value and sort the items in the array
       roster[key].sort! # And replace them in the roster (key order does not matter)
     end
+=======
+    roster.sort
+    binding.pry
+    # roster.keys.each do |key,values|
+    #   roster[9].sort
+    # end
+      # roster[9].sort
+>>>>>>> 13a2e7aa07e679143290145b332411e4e80acd9b
   end
 end
