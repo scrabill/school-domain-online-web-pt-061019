2103ab2202723d7bbb34df784d32d95e3380a6a4
scrabill <shannon.crabill@gmail.com>
2019-09-27 18:32:35 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -10,22 +10,6 @@ class School
     @roster = {} # Note that roster is a hash, not an array
   end
 
-  # def name=(name)
-  #   @name = name
-  # end
-  #
-  # def name
-  #   @name
-  # end
-  #
-  # def roster=(roster)
-  #   @roster = roster
-  # end
-  #
-  # def roster
-  #   @roster
-  # end
-
   def add_student(name,grade)
 
     if roster.has_key?(grade) # If the roster already has a key, with the given grade...
@@ -34,8 +18,7 @@ class School
       roster[grade] = [] # Otherwise, create a new key, using that grade...
       roster[grade] << name # And add the student
     end
-
-    # hash["new_key"] = "New Value"
+    
   end
 
   def grade(grade) # Given a specifc key (grade)...
@@ -43,20 +26,8 @@ class School
   end
 
   def sort
-# <<<<<<< HEAD
-    # roster.sort
-    # binding.pry
     roster.each do |key,value| # For each key, take the value and sort the items in the array
       roster[key].sort! # And replace them in the roster (key order does not matter)
     end
-# =======
-#     roster.sort
-#     binding.pry
-#     # roster.keys.each do |key,values|
-#     #   roster[9].sort
-#     # end
-#       # roster[9].sort
-# >>>>>>> 13a2e7aa07e679143290145b332411e4e80acd9b
-#   end
-end
+  end
 end
